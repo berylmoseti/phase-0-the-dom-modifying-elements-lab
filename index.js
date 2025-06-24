e1c3af95e1c3bfee335685d43bc996f14d697dd6
beryl-hub-ai <nyambekimoseti@gmail.com>
2025-06-24 14:57:03 +0300

    modifyingm

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+
+const main = document.getElementById("main");
+if (main) {
+  main.remove();
+}
+
+
+const newHeader = document.createElement("h1");
+
+
+newHeader.id = "victory";
+
+
+newHeader.textContent = "Beryl Nyambeki is the champion";
+
+
+document.body.append(newHeader);
